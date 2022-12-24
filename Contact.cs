fbbd091fba63c7633824fb9d1410f17250df52b5
Abhibagad <abhishekbagad9@gmail.com>
2022-12-24 05:48:32 +0530

    Adding Contact

---
+++ Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADDRESSBOOKSYSTEM
+{
+    public class Contact
+    {
+
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+
+
+
+    }
+}
