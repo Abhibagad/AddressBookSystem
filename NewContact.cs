b9962b7e6ce9716d80c74e9941bf901bb5a82fd0
Abhibagad <abhishekbagad9@gmail.com>
2022-12-24 05:57:56 +0530

    Add New Contact

---
+++ NewContact.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ADDRESSBOOKSYSTEM
+//{
+//    public class NewContact
+//    {
+
+//        private static void AddNewContact()
+//        {
+
+//            NewContact contact = new NewContact();
+            
+
+//            Console.Write("Enter First Name: ");
+//            NewContact.FirstName = Console.ReadLine();
+
+//             Console.Write("Enter Last Name: ");
+//            NewContact.LastName = Console.ReadLine();
+
+//            Console.Write("Enter Address 1: ");
+//            string[] addresses = new string[2];
+//            addresses[0] = Console.ReadLine();
+//            NewContact.Addresses = addresses;
+
+//            Console.Write("Enter City: ");
+//            NewContact.City = Console.ReadLine();
+
+//            Console.Write(ZipCode: ",);
+//            NewContact.ZipCode = Console.ReadLine();
+
+//            Console.Write("Enter Phone Number: ");
+//            NewContact.PhoneNumber = Console.ReadLine();
+
+//            Console.Write("Enter Email: ");
+//            string[] Email = Console.ReadLine();
+
+
+
+//            Contact.Add(NewContact);
+//        }
+//    }
+//}
