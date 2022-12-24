e78c2e58903b252cfd36d33aecf4fd6eb293ef4a
Abhibagad <abhishekbagad9@gmail.com>
2022-12-24 06:15:20 +0530

    Delete Per

---
+++ DeletePerson.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ADDRESSBOOKSYSTEM
+//{
+//    public class Person
+//    {
+//        public static void main(String[] args)
+//        {
+//            Console.WriteLine("Are you sure you want to remove this person from your address book? (Y/N)");
+//            PrintPerson(Person);
+
+//            if (Console.ReadKey().Key == ConsoleKey.Y)
+//            {
+//                People.Remove(Person);
+//                Console.WriteLine("Person removed. Press any key to continue.");
+//                Console.ReadKey();
+//            }
+//        }
+            
+        
+//    }
+//}
